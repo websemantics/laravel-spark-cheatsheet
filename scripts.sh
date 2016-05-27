cc9c8b9d9c322a5599e2227b2608fe8450b23aab
Adnan <adnan@websemantics.ca>
2016-05-26 23:45:14 -0400

    Setup semantic.json before installing semantic-gulp

--- scripts.sh
+++ scripts.sh
@@ -8,11 +8,21 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 # Read the entered command
 command=${1:-empty}
 
-if [ $command == install ]
+if [ $command == preinstall ]
 	then
 
 		# -----------------------------------------------------------------
-		# (1) install, link with semantic-ui less styles
+		# (1) pre-install, setup semantic.json
+		# -----------------------------------------------------------------
+
+    printf '%s\n' "Create semantic.json file @ node_modules/semantic-ui"
+    echo '{"base": "","paths":{"source":{"config":"src/theme.config","definitions":"src/definitions/","site":"src/site/","themes":"src/themes/"},"output":{"packaged":"dist/","uncompressed":"dist/components/","compressed":"dist/components/","themes":"dist/themes/"},"clean":"dist/"},"permission":false,"rtl":false}' > "$BASEDIR/node_modules/semantic-ui/semantic.json"
+
+elif [ $command == install ]
+	then
+
+		# -----------------------------------------------------------------
+		# (2) install, link with semantic-ui less styles
 		# -----------------------------------------------------------------
 
     printf '%s\n' "Create semantic-ui theme config file @ node_modules/semantic-ui/src/theme.config"
@@ -21,21 +31,13 @@ if [ $command == install ]
 @import "resources/assets/less/semantic/theme.config";
 /* End Config */' > "$BASEDIR/node_modules/semantic-ui/src/theme.config"
 
-    printf '%s\n' "All done, .."
-
 elif [ $command == deploy ]
 	then
 
     # ---------------------------------------------------------------------------
-		# (2) Deploy, the following bash will deploy this app to the repo gh-page
+		# (3) Deploy, the following bash will deploy this app to the repo gh-page
 		# ---------------------------------------------------------------------------
-
-		npm install <<EOF
-yes
-
-yes
-
-EOF
+		npm install
 		npm run build
 
     # Create a new Git repo in public folder
@@ -58,8 +60,8 @@ EOF
   else
 
 		# -----------------------------------------------------------------
-		# (3) Nothing, show something .. anything ..
+		# (4) Nothing, show something .. anything ..
 		# -----------------------------------------------------------------
 
-		printf '%s\n%s\n' "Please type a valid command option:" "(1) install or (2) deploy"
+		printf '%s\n%s\n' "Please type a valid command option:" "(1) preinstall ,(2) install or (3) deploy"
 fi
