a5b7ec32a8068b47963fe8d7e35693d185efbb37
Adnan <adnan@websemantics.ca>
2016-05-27 15:54:13 -0400

    Fixing deply script for Travis

--- scripts.sh
+++ scripts.sh
@@ -8,24 +8,11 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 # Read the entered command
 command=${1:-empty}
 
-if [ $command == preinstall ]
+if [ $command == install ]
 	then
 
 		# -----------------------------------------------------------------
-		# (1) pre-install, setup semantic-ui
-		# -----------------------------------------------------------------
-
-    printf '%s\n' "Create semantic.json file and semantic folder for quite semantic-ui install"
-
-		mkdir -p "$BASEDIR/semantic/src/definitions"
-		echo '' > "$BASEDIR/semantic/src/theme.config"
-		echo '{"base":"semantic/","paths":{"source":{"config":"src/theme.config","definitions":"src/definitions/","site":"src/site/","themes":"src/themes/"},"output":{"packaged":"dist/","uncompressed":"dist/components/","compressed":"dist/components/","themes":"dist/themes/"},"clean":"dist/"},"permission":false,"rtl":false}' > "$BASEDIR/semantic.json"
-
-elif [ $command == install ]
-	then
-
-		# -----------------------------------------------------------------
-		# (2) install, link with semantic-ui less styles
+		# (1) install, link with semantic-ui less styles
 		# -----------------------------------------------------------------
 
     printf '%s\n' "Create semantic-ui theme config file @ node_modules/semantic-ui/src/theme.config"
@@ -38,34 +25,40 @@ elif [ $command == deploy ]
 	then
 
     # ---------------------------------------------------------------------------
-		# (3) Deploy, the following bash will deploy this app to the repo gh-page
+		# (2) Deploy, the following bash will deploy this app to the repo gh-page
 		# ---------------------------------------------------------------------------
-		./scripts.sh preinstall
+
+		printf '%s\n' "Create semantic.json file and semantic folder for quite semantic-ui install"
+
+		mkdir -p "$BASEDIR/semantic/src/definitions"
+		echo '' > "$BASEDIR/semantic/src/theme.config"
+		echo '{"base":"semantic/","paths":{"source":{"config":"src/theme.config","definitions":"src/definitions/","site":"src/site/","themes":"src/themes/"},"output":{"packaged":"dist/","uncompressed":"dist/components/","compressed":"dist/components/","themes":"dist/themes/"},"clean":"dist/"},"permission":false,"rtl":false}' > "$BASEDIR/semantic.json"
+
 		npm install
 		npm run build
-		#
-    # # Create a new Git repo in public folder
-    # cd "$BASEDIR/public"
-    # git init
-		#
-    # # Set user details
-    # git config user.name "iAyeBot"
-    # git config user.email "iayebot@websemantics.ca"
-		#
-    # # First commit, .. horray!
-    # git add .
-    # git commit -m "Deploy to gh-pages"
-		#
-    # # Force push ...
-    # git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-		#
-    # printf '%s\n' "All done, .."
+
+    # Create a new Git repo in public folder
+    cd "$BASEDIR/public"
+    git init
+
+    # Set user details
+    git config user.name "iAyeBot"
+    git config user.email "iayebot@websemantics.ca"
+
+    # First commit, .. horray!
+    git add .
+    git commit -m "Deploy to gh-pages"
+
+    # Force push ...
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+    printf '%s\n' "All done, .."
 
   else
 
 		# -----------------------------------------------------------------
-		# (4) Nothing, show something .. anything ..
+		# (3) Nothing, show something .. anything ..
 		# -----------------------------------------------------------------
 
-		printf '%s\n%s\n' "Please type a valid command option:" "(1) preinstall ,(2) install or (3) deploy"
+		printf '%s\n%s\n' "Please type a valid command option:" "(1) install  or (2)"
 fi
